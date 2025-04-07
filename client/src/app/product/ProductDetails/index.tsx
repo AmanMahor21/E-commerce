443734189458ccc329c12956c9f980c6b48525cc
Aman <amanmahor021@gmail.com>
2025-04-07 17:48:33 +0530

    fix import Product

--- client/src/app/product/ProductDetails/index.tsx
+++ client/src/app/product/ProductDetails/index.tsx
@@ -1,5 +1,5 @@
-'use client'
-import { Product } from '@/state/types';
+'use client';
+import { Product } from '@/services/types';
 import React from 'react';
 
 interface ProductStoreProps {
@@ -7,21 +7,16 @@ interface ProductStoreProps {
 }
 
 export default function ProductDetails({ product }: ProductStoreProps) {
-
   return (
     <>
       <div className="flex justify-start gap-10 items-center">
-        <div className="text-black text-[24px] font-[600]">
-          {product.vendor?.companyName}
-        </div>
+        <div className="text-black text-[24px] font-[600]">{product.vendor?.companyName}</div>
         <div className="font-[600] border-2 text-[#202122] rounded p-1 border-[#0061F2] ">
           View Store
         </div>
       </div>
       <div className="text-[#404040] mt-4 font-[600] ">Product Description</div>
-      <div className="text-[#404040] mt-2">
-        {product.description}
-      </div>
+      <div className="text-[#404040] mt-2">{product.description}</div>
     </>
   );
 }
