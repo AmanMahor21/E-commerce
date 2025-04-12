a8afce33175fc42f836fcf9f228b41585d592ca6
Aman <amanmahor021@gmail.com>
2025-04-12 20:16:06 +0530

    set httponly false

--- server/src/controllers/StorefrontCustomerController.ts
+++ server/src/controllers/StorefrontCustomerController.ts
@@ -188,14 +188,14 @@ export class StorefrontController {
         const encryptedRefershToken = Crypto.AES.encrypt(refreshToken, process.env.CRYPTO_SECRET).toString();
         // const uniqueCookieName = '_x' + Math.random().toString(36).substring(2, 5);
         response.cookie('_Tt', encryptedAccessToken, {
-          httpOnly: true,
+          httpOnly: false,
           secure: process.env.NODE_ENV == 'production',
           sameSite: process.env.NODE_ENV == 'production' ? 'none' : 'lax',
           domain: process.env.NODE_ENV === 'production' ? '.vercel.app' : undefined, // Fixed!
           maxAge: 60 * 24 * 60 * 60 * 1000,
         });
         response.cookie('_Trt', encryptedRefershToken, {
-          httpOnly: true,
+          httpOnly: false,
           secure: process.env.NODE_ENV == 'production',
           sameSite: process.env.NODE_ENV == 'production' ? 'none' : 'lax',
           domain: process.env.NODE_ENV === 'production' ? '.vercel.app' : undefined, // Fixed!
